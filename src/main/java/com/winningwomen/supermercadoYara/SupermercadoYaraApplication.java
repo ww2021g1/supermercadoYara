884975debfc9375ae58b45a70e285f5f39b29a7d
vivianemk <kikulevmk@gmail.com>
2021-11-12 16:35:45 -0300

    segundo commit

--- src/main/java/com/winningwomen/supermercadoYara/SupermercadoYaraApplication.java
+++ src/main/java/com/winningwomen/supermercadoYara/SupermercadoYaraApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SupermercadoYaraApplication {
-
+	
+	//main class
 	public static void main(String[] args) {
 		SpringApplication.run(SupermercadoYaraApplication.class, args);
 	}
